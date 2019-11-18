f2112b4ed54e9d7f45f5f4085f08cc0763d6b5e6
John D. Lewis <balain@users.noreply.github.com>
2019-11-17 20:23:29 -0500

    Enhance process script to capture archive and fingerprint

--- process.sh
+++ process.sh
@@ -1,1 +1,17 @@
+#!/bin/sh
+
+filename=pmi_membership.sqlite
+
+current_time=$(date "+%Y%m%d-%H%M%S")
+echo "Timestamp: $current_time"
+
 php fetch.php && sqlite3 pmi_membership.sqlite < ingest.sql
+
+md5sum $filename > $filename.md5sum
+
+mkdir archive 2>/dev/null
+
+newfilename=$filename-$current_time
+echo "New filename: $newfilename"
+
+cp $filename archive/$newfilename
